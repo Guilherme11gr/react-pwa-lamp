6eb0e19260ef1650ed564720bf535c1b44469e99
Guilherme11gr <guilherme11.gr@gmail.com>
2019-05-14 22:16:50 -0300

    Updates

---
+++ precache-manifest.c9b3ce29015ef875dfd46132533fe3dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f35369813f78b13fc9e010f092679f6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da1cfb5d278cd4f14209",
+    "url": "/static/css/main.ee7cbe69.chunk.css"
+  },
+  {
+    "revision": "4c50f15f2300f3d9ce82",
+    "url": "/static/js/2.c3bc6166.chunk.js"
+  },
+  {
+    "revision": "da1cfb5d278cd4f14209",
+    "url": "/static/js/main.f6789946.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c722f5009d8e84ac37941164ad78ddf6",
+    "url": "/static/media/lamp-off.c722f500.svg"
+  },
+  {
+    "revision": "41b4ebc85358901adae6c1d8d836b221",
+    "url": "/static/media/lamp-on.41b4ebc8.svg"
+  }
+]);
