3f670087bef449030524831640cc76ce62223386
Guilherme11gr <guilherme11.gr@gmail.com>
2019-05-17 00:19:25 -0300

    Adicionando lampada como svg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import Lamp from './components/lamp/lamp';
+import NewLamp from './components/lamp/newLamp';
 import './App.scss';
+import Lamp from './components/lamp/lamp';
 
 function App() {
   const [lampOn, setLampOn] = useState(false);
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="container">
       <div onClick={() => setLampOn(!lampOn)}>
+        <NewLamp on={lampOn} />
         <Lamp on={lampOn} />
       </div>
     </div>
