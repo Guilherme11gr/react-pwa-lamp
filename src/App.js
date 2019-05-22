386c115a9ee2c9987afd0a38fb596961b565c0d1
Guilherme11gr <guilherme11.gr@gmail.com>
2019-05-22 09:54:28 -0300

    Adiçao do link para o repositorio

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ function App() {
   const [lampOn, setLampOn] = useState(false);
 
   MQTT.mqttClient.onMessageArrived = (message) => {
-    const onMsgArrivedMessage = `${message.destinationName} - ${message.payloadString}`;
-
-    console.log(onMsgArrivedMessage);
+    console.log(`${message.destinationName} - ${message.payloadString}`);
 
     setLampOn(message.payloadString === '1');
   }
@@ -25,7 +23,13 @@ function App() {
         <h2 style={{ textAlign: 'center', color: '#efefef' }}>
           {MQTT.isConnected}
         </h2>
+
       </div>
+        <h3 className="fade-in-bottom" style={{ textAlign: 'center', color: '#efefef' }}>
+          <a style={{ color: '#efefef' }} href="https://github.com/brain-machine/minicurso-aws-2019">
+           Ver no Git
+          </a>
+        </h3>
     </div>
   );
 }
